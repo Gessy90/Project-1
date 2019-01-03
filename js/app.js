12eedc69b3732064528493b3e86b5551fd6ed18b
Gessy90 <gessicasantoro90@hotmail.it>
2019-01-03 16:35:31 +0000

    roll dice button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 document.addEventListener( 'DOMContentLoaded', () =>{
-const h1 = document.querySelector('h1')
-h1.textContent ='Hello World!'
+  const h1 = document.querySelector('h1')
+  h1.textContent ='Snake and Ladder!'
+
+  //function to make the dice roll
+  window.rollDice =()=> {
+    const max = 6
+    const roll = Math.ceil(Math.random() * max)
+    console.log(roll)
+  }
+
 })
