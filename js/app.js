e35b0da0542cf3f228e7122ee32d0a873910eadd
Gessy90 <gessicasantoro90@hotmail.it>
2019-01-08 15:27:53 +0000

    both players are working now and we have a wincondition too

--- js/app.js
+++ js/app.js
@@ -1,63 +1,62 @@
 document.addEventListener( 'DOMContentLoaded', () =>{
-  let currentPosition = 0
   const squares = document.querySelectorAll('.squares')
-
 
   const ladsnakes = [{ start: 3, end: 8 }, { start: 12, end: 18 } , {start: 14, end: 15 } , { start: 23, end: 24 }]
   const h1 = document.querySelector('h1')
   const button = document.querySelector('button')
+  // const button2 = document.getElementById('reset')
   h1.textContent ='Snails and Ladders!'
 
   button.addEventListener('click', rollDice)
-  const player = { name: 'Player', position: 0, color: 'white' , hasWon: false}
-  const player2 = { name: 'Computer', position: 0, color: 'orange' , hasWon: false}
-  let currentPlayer = player
 
+  let player1position = 0
+  let player2position = 0
+
+  let player1turn = true
 
   function rollDice () {
     const max = 6
     const roll = Math.ceil(Math.random() * max)
-    squares[currentPosition].classList.remove('currentPosition')
-    currentPosition = currentPosition + roll
+    // const currentPlayerPostion = player1turn ? player1position : player2position
 
-    console.log('currentPosition',currentPosition)
-    console.log('squares',squares)
-    console.log('squares[currentPosition]',squares[currentPosition])
+    //both players start at 0 and player1 is true so change it to false so that player2 can have a go also
+    //and afterward player1turn changes to true so that player1 can have a go again
+    if (player1turn){
+      player1position = player1position + roll
+      player1turn = false
+    } else {
+      player2position = player2position + roll
+      player1turn = true
+    }
+
+    // player1turn = !player1turn
     // ---> Check all the ladders and see if the currentPosition is the start of any ladders
     ladsnakes.forEach( ladsnakes => {
-      if (ladsnakes.start === currentPosition) {
+      if (ladsnakes.start === player1position) {
         console.log('SNAKE OR LADDER')
-        currentPosition = ladsnakes.end
+        player1position = ladsnakes.end
+      }
+      if (ladsnakes.start === player2position) {
+        console.log('SNAKE OR LADDER')
+        player2position = ladsnakes.end
       }
     })
+    console.log(roll)
+    console.log(player1position, player2position)
+    if (player1position >= 24 || player2position >= 24) winConditions()
+  }
 
-    const turn = true
-
-    if (turn === true){
-      // player needs to move
-    } else if (turn === false){
-      // player2 needs to move
-    }
-    //when dice is rolled the player needs to move accordingly
-
-
-    //to make the player move and win at the end
-    if (currentPosition < 24) {
-      squares[currentPosition].classList.add('currentPosition')
-    } else{
-      alert(currentPosition + ' I won!')
-      currentPosition = true
-    }
-
-
-    if (currentPlayer === player) {
-      currentPlayer = player2
+  // function that checks to see whether a player has won every time they role.
+  function winConditions() {
+    if (player1position < 24) {
+      squares[player1position].classList.add('currentPosition')
     } else {
-      currentPlayer = player2
+      (player1position === 24)
+      return alert('Player 1 is the winner')
     }
+    (player2position === 24)
+    return alert('Player 2 is the winner')
   }
 
 
 })
-//when touches snails needs to go back 1 square.
-//when touches ladder needs to shoot up down and lands it on the space next to it.
