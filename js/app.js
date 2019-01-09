22664c99e19ee5062e928472304e388ecd814aff
Gessy90 <gessicasantoro90@hotmail.it>
2019-01-09 13:39:16 +0000

    added a button that now bounces and made the grid smaller and centered

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 document.addEventListener( 'DOMContentLoaded', () =>{
   const squares = document.querySelectorAll('.squares')
 
-  const ladsnakes = [{ start: 3, end: 8 }, { start: 12, end: 18 } , {start: 14, end: 15 } , { start: 23, end: 24 }]
+  const ladsnakes = [{ start: 3, end: 8 }, { start: 12, end: 19 } , {start: 14, end: 15 } , { start: 25, end: 26 }, {start: 30, end: 34 } , { start: 36, end: 40 }]
   const h1 = document.querySelector('h1')
   const button = document.querySelector('button')
   const btnReset = document.getElementById('reset')
   const diceSquare = document.getElementById('dice')
+  const dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;']
 
   h1.textContent ='Snails and Ladders!'
 
@@ -19,28 +20,12 @@ document.addEventListener( 'DOMContentLoaded', () =>{
   squares[player2position].classList.add('player2')
 
   let player1turn = true
-  //make actual dice roll
-  const dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;']
-  // let stop = true
-  // let t
 
-
-  // function stopstart() {
-  //   if(stopped) {
-  //     stopped = false
-  //     t = setInterval(change, 100)
-  //   } else {
-  //     clearInterval(t)
-  //     stopped = true
-  //   }
-  // }
 
   function rollDice () {
     const max = 6
     const roll = Math.ceil(Math.random() * max)
     diceSquare.innerHTML = dices[roll-1]
-
-    // const currentPlayerPostion = player1turn ? player1position : player2position
 
     //both players start at 0 and player1 is true so change it to false so that player2 can have a go also
     //and afterward player1turn changes to true so that player1 can have a go again
@@ -57,7 +42,7 @@ document.addEventListener( 'DOMContentLoaded', () =>{
     }
 
     console.log(squares[player1position])
-    // player1turn = !player1turn
+
     // ---> Check all the ladders and see if the currentPosition is the start of any ladders
     ladsnakes.forEach( ladsnakes => {
       if (ladsnakes.start === player1position) {
@@ -68,7 +53,9 @@ document.addEventListener( 'DOMContentLoaded', () =>{
         console.log('SNAKE OR LADDER')
         player2position = ladsnakes.end
       }
+
     })
+
     console.log(roll)
     console.log(player1position, player2position)
     if (player1position >= 38 || player2position >= 38) winConditions()
