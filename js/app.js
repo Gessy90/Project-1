886fcffef701e3607796ce38e79ce57e4885498e
Gessy90 <gessicasantoro90@hotmail.it>
2019-01-10 13:36:33 +0000

    added snails and tunnels in the grid accordingly and reset button is working now

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener( 'DOMContentLoaded', () =>{
   const squares = document.querySelectorAll('.squares')
 
-  const ladsnakes = [{ start: 3, end: 8 }, { start: 12, end: 19 } , {start: 14, end: 15 } , { start: 25, end: 26 }, {start: 30, end: 34 } , { start: 36, end: 40 }]
+  const ladsnakes = [{ start: 2, end: 7 }, { start: 12, end: 19 } , {start: 29, end: 34 } , { start: 9, end: 8 } , { start: 17, end: 16 }, { start: 22, end: 21 } , {start: 24, end: 23 }, { start: 32, end: 31 } , {start: 39, end: 38 }]
   const h1 = document.querySelector('h1')
   const button = document.querySelector('button')
   const btnReset = document.getElementById('reset')
@@ -9,11 +9,9 @@ document.addEventListener( 'DOMContentLoaded', () =>{
   const dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;']
 
 
-  const roll = 0
+  h1.textContent ='Snails and Tunnels.'
 
-  h1.textContent ='Snails and Ladders!'
-
-  button.addEventListener('click', rollDice)
+  button.addEventListener('click', waitThenRoll)
   btnReset.addEventListener('click', theEnd)
 
   let player1position = 0
@@ -24,10 +22,17 @@ document.addEventListener( 'DOMContentLoaded', () =>{
 
   let player1turn = true
 
+  function waitThenRoll(){
+    diceSquare.innerHTML = ''
+    setTimeout(() =>{
+      rollDice()
+    }, 1000)
+  }
+
 
   function rollDice () {
     const max = 6
-    roll = Math.ceil(Math.random() * max)
+    const roll = Math.ceil(Math.random() * max)
     diceSquare.innerHTML = dices[roll-1]
 
     //both players start at 0 and player1 is true so change it to false so that player2 can have a go also
@@ -39,9 +44,11 @@ document.addEventListener( 'DOMContentLoaded', () =>{
 
     if (player1turn){
       player1position = player1position + roll
+      if (player1position >= 41) winConditions()
       player1turn = false
     } else {
       player2position = player2position + roll
+      if (player2position >= 41) winConditions()
       player1turn = true
     }
 
@@ -65,32 +72,37 @@ document.addEventListener( 'DOMContentLoaded', () =>{
 
     console.log(roll)
     console.log(player1position, player2position)
-    if (player1position >= 40 || player2position >= 40) winConditions()
+    // if (player1position >= 41 || player2position >= 41) winConditions()
   }
-//function to set interval so that i can see all the players moves
+  //function to set interval so that i can see all the players moves
 
-let time remaining = 5
-
-const timerId = setInterval(() => {
-    div.classList.remove('player')
-    console.log(timeRemaining)
-    div = divs[Math.floor(Math.random() * divs.length)]
-    div.classList.add('player')
-    if(timeRemaining === 0) clearInterval(timerId)
-  }, 1000)
-
+  // const setTimeOut = 5
+  //
+  // function randomTime(min,max){
+  //   return Math.round(Math.random()* (min-max)+ min)
+  // }
+  //
+  // function jump(){
+  //   const time =randomTime(200,1000)
+  //   squares[player1position].classList.add('player')
+  //   setTimeOut(() =>{
+  //     squares[player1position].classList.remove('player')
+  //   })
+  // }
 
 
   // function that checks to see whether a player has won every time they role.
   function winConditions() {
-    if (player1position < 40) {
-      squares[player1position].classList.add('currentPosition')
-    } else {
-      (player1position === 40)
+    if (player1position >= 41) {
+      player1position = 41
+      theEnd()
       return alert('Player is the winner')
+    } else if (player2position >= 41)  {
+      player2position = 41
+      theEnd()
+      return alert('Computer is the winner')
     }
-    (player2position === 40)
-    return alert('Computer is the winner')
+
   }
 
 
@@ -107,4 +119,3 @@ const timerId = setInterval(() => {
 //go backward when touches the Snails
 //when playing for the second time there is a problem with the button rollDice
 //when i reset the game the big dice doesn't disappear but it should
-//reset button word needs to be centered
